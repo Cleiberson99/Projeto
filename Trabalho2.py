338ee92667cce66ecc817de5df87e0acaaad9ef5
Cleiberson99 <lucasfabiano807@gmail.com>
2026-01-14 11:25:21 -0300

    Remove all code from Trabalho2.py
    ocorreu um erro aqui e o codigo do 1 se juntou com o 2

--- Trabalho2.py
+++ Trabalho2.py
@@ -1,10 +1,1 @@
-from tkinter import *
-from tkinter import ttk
-import sqlite3
-root = Tk()
-root.geometry("800x600")
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Game world").grid(column=7, row=7)
 
-root.mainloop()
